54208f0d467d3e827996f85f937f76bc9a3b3cb8
sbx_user1051 <sbx_user1051@169.254.6.189>
2019-05-01 15:28:30 +0000

    Wed May 01 15:28:30 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,12 +3,15 @@ package befaster.solutions.FIZ;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        String res = number.toString();
-        
+    	if(number % 15 == 0) {return "fizz buzz";}
+        String res = number % 3 == 0 ? "fizz" : number.toString();
+        if(number % 15 == 0) res += " ";
+        if(number % 5 == 0) res = "buzz";
         return res;
     }
 
 }
+
 
 
 
